3781bc0ee1754ce39fab72a593fee47cbc4e0765
Pulkit Saxena <pulkit.s@calidig.com>
2024-06-08 15:30:55 +0530

    Desgino Navbar complete

--- Designo/src/components/Navbar.jsx
+++ Designo/src/components/Navbar.jsx
@@ -5,60 +5,66 @@ import CloseIcon from "@mui/icons-material/Close"
 import { useState } from "react"
 
 const navItems = [
-  { id: 1, link: "Our Compnay" },
-  { id: 2, link: "Locations" },
-  { id: 3, link: "Contact" },
+  { id: 1, link: "Home", to: "/" },
+  { id: 2, link: "Our Compnay", to: "/About" },
+  { id: 3, link: "Locations", to: "/Location" },
+  { id: 4, link: "Contact", to: "/Contact" },
 ]
 
 export const Navbar = () => {
   const [open, setOpen] = useState(false)
   return (
-    <div className="flex flex-col gap-8 md:flex-row justify-between items-center lg:text-xl p-4 uppercase">
-      <div className="flex justify-between w-full">
-        {/* Logo Div */}
-        <div className="flex gap-4 items-center">
-          <FiberSmartRecordIcon fontSize="medium" />
-          <h1 className="sm:text-2xl lg:text-3xl font-bold transition-all">
-            Designo
-          </h1>
+    <nav className="w-full fixed top-0 left-0 p-4">
+      <div className="md:flex bg-white p-4 items-center justify-between">
+        {/* Logo DIv */}
+        <div className="flex items-center justify-between">
+          {/* Icon and title Div */}
+          <div className="flex items-center gap-2">
+            <FiberSmartRecordIcon fontSize="medium" />
+            <span className="sm:text-2xl lg:text-3xl font-bold transition-all">
+              Designo
+            </span>
+          </div>
+          {/* Menu Icons */}
+          <div
+            onClick={() => {
+              setOpen(!open)
+            }}
+            className="md:hidden"
+          >
+            {open ? (
+              <CloseIcon
+                className="cursor-pointer text-gray-400 hover:text-black transition-all"
+                fontSize="large"
+              />
+            ) : (
+              <MenuIcon
+                className="cursor-pointer text-gray-400 hover:text-black transition-all"
+                fontSize="large"
+              />
+            )}
+          </div>
         </div>
 
-        {/* Hamburger Div */}
-        <div
-          onClick={() => {
-            setOpen(!open)
-          }}
-          className="md:hidden"
+        {/* Nav Links */}
+        <ul
+          className={`flex flex-col md:flex-row gap-4 md:gap-6 md:items-center py-6 md:py-0 text-left text-xl md:text-2xl text-gray-400 transition-all duration-200 absolute md:static md:z-auto z-[-1] w-full md:w-auto ${
+            open ? "top-20 opacity-100" : "top-[-490px]"
+          } md:opacity-100`}
         >
-          {open ? (
-            <CloseIcon
-              className="cursor-pointer text-gray-400 hover:text-black transition-all"
-              fontSize="large"
-            />
-          ) : (
-            <MenuIcon
-              className="cursor-pointer text-gray-400 hover:text-black transition-all"
-              fontSize="large"
-            />
-          )}
-        </div>
+          {navItems.map((link) => {
+            return (
+              <Link
+                to={link.to}
+                className="hover:text-black hover:scale-105 transition-all duration-200 font-medium w-fit"
+                key={link.id}
+              >
+                {link.link}
+              </Link>
+            )
+          })}
+        </ul>
       </div>
-      <ul
-        className={`flex flex-col md:flex-row gap-4 lg:gap-8 text-gray-400 cursor-pointer md:text-lg whitespace-nowrap ${
-          open ? "visible" : "hidden"
-        }`}
-      >
-        {navItems.map((link) => {
-          return (
-            <Link
-              className="hover:text-black hover:scale-105 transition-all duration-200 font-medium"
-              key={link.id}
-            >
-              {link.link}
-            </Link>
-          )
-        })}
-      </ul>
-    </div>
+    </nav>
   )
 }
