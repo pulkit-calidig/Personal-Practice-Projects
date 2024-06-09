7036f95a30556d0e1591a39861b4a88d4634154d
Pulkit Saxena <pulkit.s@calidig.com>
2024-06-09 23:31:18 +0530

    Setup for backend N created first ever api

---
+++ Designo/src/backend/server.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef */
+import express from "express"
+import mysql from "mysql"
+import cors from "cors"
+import * as dotenv from "dotenv"
+dotenv.config()
+
+const app = express()
+app.use(cors())
+
+// PORT address
+const PORT = process.env.PORT
+
+// * Connection to mysql db
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "designo",
+})
+
+// * api call for data
+app.get("/categories", (req, res) => {
+  const sql = "SELECT * FROM Categories"
+  db.query(sql, (err, data) => {
+    if (err) return res.json(err)
+    return res.json(data)
+  })
+})
+
+app.get("/", (req, res) => {
+  return res.json("From the backend")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
